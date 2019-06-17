6927ec95e653db77f86cd976159641eea33c8dc1
MishaPepyaka <MishaPepyaka@gmail.com>
2019-06-17 13:58:46 +0300

    main layout added, added views, routes updated.

--- pepka.js
+++ pepka.js
@@ -2,38 +2,34 @@ var express = require('express');
 
 // Установка шаблонизатора handlebars
 
+var app = express();
+
 var handlebars = require('express-handlebars')
     .create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine','handlebars');
 
-var app = express();
-
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
-    res.type('text/plain');
-    res.send('Pepka сайт');
+    res.render('home');
 });
 
 app.get('/about', function (req, res) {
-    res.type('text/plain');
-    res.send('О Pepka');
+    res.render('about');
 });
 
 // пользовательская страница 404
 app.use(function (req, res) {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Не найдено');
+    res.render('404');
 });
 
 // пользовательская страница 500
 app.use(function (err, req, res, next) {
     console.log(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Ошибка сервера');
+    res.render('404');
 });
 
 app.listen(app.get('port'), function () {
